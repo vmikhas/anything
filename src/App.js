aa12a6008438835f716b0ec9fbf3897f067b2559
vmikhas <vmikhas@yandex.ru>
2024-08-23 18:43:46 +0300

    переключение между слайдами и экранами по пагинации + навигации, бургер для мобилки + меню

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import {BrowserRouter} from 'react-router-dom';
 import './style.scss';
 import {Router} from './router/router';
-import {useSelector} from 'react-redux';
+// import {useSelector} from 'react-redux';
 
 // import { SwitchTransition, CSSTransition} from "react-transition-group";
 
 
 function App() {
 
-	const {currentPage, slideIndex, activeScreen} = useSelector((state) => state.content);
+	// const {currentPage, slideIndex, activeScreen} = useSelector((state) => state.content);
 
-	console.log('currentPage', currentPage);
-	console.log('slideIndex', slideIndex);
-	console.log('activeScreen', activeScreen);
-	console.log('______________________');
+	// console.log('currentPage', currentPage);
+	// console.log('slideIndex', slideIndex);
+	// console.log('activeScreen', activeScreen);
+	// console.log('______________________');
 
 	return (
 		<div className={"App"} style={{ maxHeight: '1080rem', height: '100%' }}>
